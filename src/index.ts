c64e06dc867609619f5e19517246a6262d9188ea
Oleksii Yesin <Oleksii_Yesin@epam.com>
2022-08-19 14:35:41 +0300

    setup action2

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,39 @@
-import { getInput } from "@actions/core"
+const core = require('@actions/core');
+const fs = require('fs');
+const path = require('path');
+const dir = core.getInput('dir');
+const stateFile = 'terraform.tfstate';
 
-const inputName = getInput("name");
+async function run() {
+    try {    
+        if (fs.existsSync(`${dir}./${stateFile}`)) {
+            console.log("continue");
+        } else {
+            console.log("stop");
+        }
+    }
+    catch (e){
+        core.console.error("failed");        
+    }
 
-greet(inputName)
-
-function greet(name: string) {
-    console.log(`Hello ${name}`);
+    try {   
+        let stateFiles = fs.readFileSync(`${dir}/${stateFile}`);
+        let obj = JSON.parse(stateFiles);
+        const shareInfoLen = Object.keys(obj.resources).length; 
+        const maxAttempts = 3;
+        let attempt = 0;
+        while (attempt < maxAttempts) {
+            attempt++
+            if (shareInfoLen != 0) {
+                console.log(`Destroying terraform attempt ${attempt}`);
+            } else {
+                console.log(`Not resources on attempt ${attempt}`);
+            }
+        }
+    }
+    catch(err) {
+        console.log(err);
+    }
 }
+
+run();
