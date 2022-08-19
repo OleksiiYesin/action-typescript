3efaeacd7cbe2d4bd43c71a3740727cbd04482f0
Oleksii Yesin <Oleksii_Yesin@epam.com>
2022-08-19 14:03:51 +0300

    setup input

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,9 @@
-console.log("Hello world");
+import { getInput } from "@actions/core"
+
+const inputName = getInput("name");
+
+greet(inputName)
+
+function greet(name: string) {
+    console.log(`Hello ${name}`);
+}
