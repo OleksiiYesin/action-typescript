b9a68cd080d722d4db593b73e06ac7702e61f866
Oleksii Yesin <Oleksii_Yesin@epam.com>
2022-08-22 14:56:45 +0300

    WIP

--- src/index.ts
+++ src/index.ts
@@ -1,52 +1,52 @@
-import { error, getInput, info, setFailed, warning } from '@actions/core'
-import {getStdOutput} from './res/utils';
 import * as fs from 'fs';
+import { execSync } from 'child_process';
+import * as core from '@actions/core';
 
-const workDir: string  = getInput('work_dir').trim();
-const maxAttempts: any = getInput('max_attempts').trim();
-const dryRun: any      = getInput('dry_run').trim();
 
-( async () => {
+const dir               = core.getInput('work_dir');
+const maxAttempts: any  = core.getInput('max_attempts');
+const dryRun: any       = core.getInput('dry_run');
+const state             = 'terraform.tfstate'
 
+async function run() {
     try {
-        const state = 'terraform.tfstate'
-        if (fs.existsSync(`${workDir}/${state}`))
+        if (fs.existsSync(`${dir}/${state}`))
             await destroy(); 
     }
     catch(e) {
-        error('\n File not exist')
+        core.error('\n File not exist')
         throw e;
     }
+}
 
 async function destroy() {
     try {
         // parsing terraform.tfstate file
-        const state = 'terraform.tfstate'
-        const stateFile = fs.readFileSync(`${workDir}/${state}`);
+        const stateFile = fs.readFileSync(`${dir}/${state}`);
         const obj = JSON.parse(stateFile.toString());
         const shareInfoLen = Object.keys(obj.resources).length;
         // destroying resources
-        let destroyResources = await getStdOutput(`cd ${workDir} && terraform init && terraform destroy --auto-approve`, [])
+        let destroyResources = execSync(`cd ${dir} && terraform init && terraform destroy --auto-approve`).toString();
         let attempt = 0;
         while (attempt < maxAttempts) {
             attempt++;
             if (shareInfoLen != 0) {
-              info(`Prepare for destroying: ${shareInfoLen} resources...\n`);
-              info(`\n[LOG] Destroying terraform attempt: ${attempt}`);
+              core.info(`Prepare for destroying: ${shareInfoLen} resources...\n`);
+              core.info(`\n[LOG] Destroying terraform attempt: ${attempt}`);
               if (dryRun === 0) {
-                info('[DEBUG] Taking destroy branch')
+                core.info('[DEBUG] Taking destroy branch')
                   if (destroyResources) {
-                    info(`[LOG] Resources was destroyed on: ${attempt} [${dryRun}]`)
+                    core.info(`[LOG] Resources was destroyed on: ${attempt} [${dryRun}]`)
                     break;
                   } else {
-                      warning(`[WARN] Failed to destroy: ${attempt} [${dryRun}]`)
+                      core.warning(`[WARN] Failed to destroy: ${attempt} [${dryRun}]`)
                   }
               } else {
-                  info(`[LOG] Destroyed resources on attempt: ${attempt}`)
+                  core.info(`[LOG] Destroyed resources on attempt: ${attempt}`)
                   break;
               }
             } else {
-                info(`"${state}" was already empty`)
+                core.info(`"${state}" was already empty`)
             }
         }
     }
@@ -54,4 +54,5 @@ async function destroy() {
         throw e;
     }
 }
-})();
+
+run().catch(err => core.setFailed(err.message));
