26711bbf63ebfb185631844f87b0e74d308a206a
Oleksii Yesin <Oleksii_Yesin@epam.com>
2022-08-22 16:41:24 +0300

    WIP

--- src/index.ts
+++ src/index.ts
@@ -11,12 +11,58 @@ const state             = 'terraform.tfstate'
 
 async function run() {
     try {
-        if (fs.existsSync(`${dir}/${state}`)) {
+        if (!dir && fs.existsSync(`${state}`)) {
+            await destroyOne();
+        } else {
+            setFailed('\nFile not exist in 1st case!')
+        }
+
+        if (dir && fs.existsSync(`${dir}/${state}`)) {
             await destroy(); 
         } else {
             setFailed('\nFile not exist!')
         }
         
+    }
+    catch(e) {
+        throw e;
+    }
+}
+
+async function destroyOne() {
+    try {
+        
+        // parsing terraform.tfstate file
+        const stateFile = fs.readFileSync(`${state}`);
+        const obj = JSON.parse(stateFile.toString());
+        const shareInfoLen = Object.keys(obj.resources).length;
+
+        // destroying resources
+        const destroyResources = async () => {return getStdOutput('terraform', [ 'init' ])};
+        const destroy = async () => {return getStdOutput('terraform', [ 'destroy', '--auto-approve' ])};
+        let attempt = 0;
+        
+        while (attempt < maxAttempts) {
+            attempt++;
+            if (shareInfoLen != 0) {
+              info(`Prepare for destroying: ${shareInfoLen} resources...\n`);
+              info(`\n[LOG] Destroying terraform attempt: ${attempt}...`);
+              if (dryRun === 0) {
+                info('[DEBUG] Taking destroy branch')
+                  if (await destroyResources() && await destroy()) {
+                    info(`[LOG] Resources was destroyed on: ${attempt} [${dryRun}]`)
+                    break;
+                  } else {
+                      warning(`[WARN] Failed to destroy: ${attempt} [${dryRun}]`)
+                  }
+              } else {
+                  info(`[LOG] Destroyed resources on attempt: ${attempt}`)
+                  break;
+              }
+            } else {
+                info(`"${state}" was already empty`)
+            }
+        }
     }
     catch(e) {
         throw e;
